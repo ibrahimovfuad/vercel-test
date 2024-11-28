4af8049dfd84e1e0b651ad3b7e7281e1182e9e91
ibrahimovfuad <fuad.ibrahimov@live.com>
2024-11-28 22:18:01 +0100

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -18,43 +18,53 @@ function encodeFormData(data) {
 export default async function handler(req, res) {
     if (req.method === 'POST') {
         const body = req.body;
-    
+
+        // Разделение имени и фамилии
+        const firstName = body.Input?.split(' ')[0] || '';
+        const lastName = body.Input?.split(' ')[1] || '';
+
         const headers = {
             'Accept': 'application/json',
             'Content-Type': 'application/x-www-form-urlencoded',
             'Token': '10524rjbW0MY9xAYrhBkx1wIrMzxe43nb0ZLRzeu48IKTxx9KRG8h8fZ0St3Lxzu',
             'Cabinet-Hash': '99a805bc0d0845bf489b9b09ad69ea30'
         };
-        
+
         const data = {
-            first_name: body.Input.split(' ')[0],
-            last_name: body.Input.split(' ')[1],
+            first_name: firstName,
+            last_name: lastName,
             email: body.Email,
             phone: body.Phone,
-            // tags: ['tag1'],
             send_activation_email: false,
-            additional_fields: [
+            additional_fields: body.Input_3 ? [
                 {
                     field_id: 687,
                     value: body.Input_3
                 }
-            ]
+            ] : []
         };
-    
-        await fetch('https://api.kwiga.com/contacts', {
-            method: 'POST',
-            headers: headers,
-            body: encodeFormData(data)
-        })
-            .then(response => response.json())
-            .then(result => {
-                console.log(result);
-            })
-            .catch(error => {
-                console.error('Error:', error);
+
+        try {
+            const response = await fetch('https://api.kwiga.com/contacts', {
+                method: 'POST',
+                headers: headers,
+                body: encodeFormData(data)
             });
 
-        return res.status(200).json({ message: 'Data received and processed' });
+            if (!response.ok) {
+                const errorDetails = await response.text();
+                console.error('API Error:', errorDetails);
+                return res.status(response.status).json({ message: 'Failed to process data', details: errorDetails });
+            }
+
+            const result = await response.json();
+            console.log(result);
+
+            return res.status(200).json({ message: 'Data received and processed' });
+        } catch (error) {
+            console.error('Fetch Error:', error);
+            return res.status(500).json({ message: 'Internal Server Error', error: error.message });
+        }
     } else {
         return res.status(405).json({ message: 'Method Not Allowed' });
     }
