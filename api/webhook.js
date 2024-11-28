c60d13d12e8a68b4933bd3185d353d40f585a4e1
ibrahimovfuad <fuad.ibrahimov@live.com>
2024-11-28 22:42:21 +0100

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -25,7 +25,7 @@ module.exports = async function handler(req, res) {
                 }
             ] : []
         };
-        
+        console.log(data);
         try {
             const response = await fetch('https://api.kwiga.com/contacts', {
                 method: 'POST',
